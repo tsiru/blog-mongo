dc703693272f869b102473a20294efe88e3febac
Bartłomiej Rapacz <bartlomiej.rapacz@infakt.pl>
2014-05-14 21:29:03 +0200

    Added devise

--- config/application.rb
+++ config/application.rb
@@ -41,7 +41,7 @@ module Blog
     config.encoding = "utf-8"
 
     # Configure sensitive parameters which will be filtered from the log file.
-    config.filter_parameters += [:password]
+    config.filter_parameters += [:password, :password_confirmation]
 
     # Enable escaping HTML in JSON.
     config.active_support.escape_html_entities_in_json = true
@@ -62,5 +62,15 @@ module Blog
 
     # Version of your assets, change this if you want to expire all your assets
     config.assets.version = '1.0'
+
+    config.generators do |g|
+      g.orm                 :mongoid
+      g.template_engine     :haml
+      g.test_framework      :rspec
+      g.stylesheets         false
+      g.view_specs          false
+      g.helper              false
+      g.fixture_replacement :factory_girl, dir: "spec/support/factories"
+    end
   end
 end
